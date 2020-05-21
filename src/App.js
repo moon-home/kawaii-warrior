9218412c61f666b13710bba12250bf28f901f611
moonmoonhome <moon30biology@gmail.com>
2020-05-20 21:50:51 -0400

    refactor all mapStateToProps using createStructuredSelector

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Route, Switch, Redirect } from 'react-router-dom';
 import {connect} from 'react-redux';
+import {createStructuredSelector} from 'reselect';
 import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
@@ -9,6 +10,7 @@ import Header from './components/header/header.component';
 import SigninSignupPage from './pages/signin-signup-page/signin-signup-page.component';
 import {auth, createUserProfileDocument} from './firebase/firebase.utils';
 import {setCurrentUser} from './redux/user/user.actions';
+import {selectCurrentUser} from './redux/user/user.selector';
 
 class App extends React.Component {
 
@@ -54,8 +56,8 @@ class App extends React.Component {
   }
 }
 
-const mapStateToProps = ({user}) => ({
-  currentUser: user.currentUser
+const mapStateToProps = createStructuredSelector({
+  currentUser: selectCurrentUser
 })
 
 const mapDispatchToProps = dispatch => ({
