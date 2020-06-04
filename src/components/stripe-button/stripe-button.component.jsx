941f7d76ec827c539e725a7175eb3df5909bee3c
moonmoonhome <moon30biology@gmail.com>
2020-06-04 09:16:33 -0400

    add stripe, update favicon and title

--- src/components/stripe-button/stripe-button.component.jsx
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -3,7 +3,7 @@ import StripeCheckout from 'react-stripe-checkout';
 
 const StripeCheckoutButton = ({ price }) => {
   const priceForStripe = price * 100;
-  const publishableKey = 'pk_test_WBqax2FWVzS9QlpJScO07iuL';
+  const publishableKey = 'pk_test_51GqIXSFgfcNReLmKDYLhVxweM9DdWQYnbEa9cty5kx1sneVvUEA4WhDuz8CZuGQ6qfvi5keL6FPLLshjatbo37P9008YLgYyVF';
 
   const onToken = token => {
     console.log(token);
